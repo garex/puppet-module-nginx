fc1958449367c3769b63b92ac6cd87d984767e24
ustimenko <ustimenko.alexander@gmail.com>
2012-08-17 18:21:25 +0700

    Basic virtual hosts with pre cleaning up

---
+++ manifests/site.pp
@@ -0,0 +1,27 @@
+define nginx::site (
+  $server_name,
+  $root,
+  $root_owner = undef,
+  $root_group = undef,
+  $index      = "index.html"
+) {
+
+  file {
+    "Nginx root directory for site $name":
+      path    => $root,
+      ensure  => directory,
+      owner   => $root_owner,
+      group   => $root_group;
+
+    "Nginx configuration file for site $name":
+      path    => "/etc/nginx/conf.d/$name.conf",
+      content => template("nginx/nginx.site.conf.erb"),
+      require => [
+        File["Nginx root directory for site $name"],
+        Exec["Nginx config purge"],
+        Package["nginx"]
+      ],
+      notify  => Service["nginx"];
+  }
+
+}
