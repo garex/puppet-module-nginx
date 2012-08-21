3c4c3ee8931776285f2737ed5d9a0fcbb97e5172
ustimenko <ustimenko.alexander@gmail.com>
2012-08-21 15:54:14 +0700

    Adding ruby backend as pool of thin servers

---
+++ manifests/backend/install/ruby.pp
@@ -0,0 +1,47 @@
+class nginx::backend::install::ruby {
+
+  $thin = "$(gem env gemdir)/bin/thin"
+
+  package {"Ruby gems provider":
+    name        => ["rubygems", "rake"]
+  }
+
+  package {"Ruby backend service":
+    require     => Package["Ruby gems provider"],
+    name        => "thin",
+    provider    => "gem",
+  }
+
+  exec {"Making thin bin visible":
+    require   => Package["Ruby backend service"],
+    command   => "ln --symbolic $thin /usr/bin/thin",
+    creates   => "/usr/bin/thin",
+  }
+
+  if ($::operatingsystem == debian) {
+    $persistent_service = "/usr/sbin/update-rc.d -f thin defaults;"
+  } else {
+    $persistent_service = ""
+  }
+
+  exec {"Install ruby backend service":
+    creates     => "/etc/thin",
+    command     => "thin install; $persistent_service",
+  }
+
+  file {"Ruby backend config directory should be fresh":
+      require   => Exec["Install ruby backend service"],
+      recurse   => true,
+      purge     => true,
+      path      => "/etc/thin",
+      ensure    => directory
+  }
+
+  service {"thin":
+    ensure      => running,
+    hasrestart  => true,
+    enable      => true,
+    require     => Exec["Install ruby backend service"],
+  }
+
+}
---
+++ manifests/backend/ruby.pp
@@ -0,0 +1,33 @@
+define nginx::backend::ruby (
+  $root,
+  $port       = 8000,
+  $address    = '127.0.0.1',
+  $servers    = 3,
+  $try_files  = '$uri $uri/ $uri/index.html @rubybackend'
+) {
+
+  include nginx::backend::install::ruby
+
+  File {
+    require => [
+      File["Nginx root directory for site $name"],
+      Package["nginx"]
+    ],
+    notify  => [Service["nginx"], Service["thin"]]
+  }
+
+  file {
+    "Ruby backend config for $name":
+      path    => "/etc/thin/$name.yml",
+      content => template("nginx/backend.ruby.thin.pool.yml.erb");
+
+    "Nginx ruby backend before for site $name":
+      path    => "/etc/nginx/conf.d/$name.backend.ruby.before.conf",
+      content => template("nginx/backend.ruby.before.conf.erb");
+
+    "Nginx ruby backend inside for site $name":
+      path    => "/etc/nginx/conf.d/$name.backend.ruby.inside.conf",
+      content => template("nginx/backend.ruby.inside.conf.erb");
+  }
+
+}
