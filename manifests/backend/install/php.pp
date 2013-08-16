632355e7a2292762b18a938138af350b2bcb7554
Wojciech Arabczyk <arabek@gmail.com>
2013-08-16 13:33:45 +0200

    linting

--- manifests/backend/install/php.pp
+++ manifests/backend/install/php.pp
@@ -1,47 +1,48 @@
+# /manifests/backend/install/php.pp
 class nginx::backend::install::php {
 
   # Set global defaults
   Exec {
-    path => "/usr/bin:/usr/sbin/:/bin:/sbin"
+    path => '/usr/bin:/usr/sbin/:/bin:/sbin'
   }
 
-  package {"PHP commons":
-    name    => ["php5-cli", "php5-common", "php5-suhosin"],
+  package {'PHP commons':
     ensure  => installed,
+    name    => ['php5-cli', 'php5-common', 'php5-suhosin']
   }
 
-  file {"PHP FPM pool config directory should be fresh":
-      require => Package["PHP FPM and CGI"],
+  file {'PHP FPM pool config directory should be fresh':
+      ensure  => directory,
       recurse => true,
       purge   => true,
-      path    => "/etc/php5/fpm/pool.d",
-      ensure  => directory
+      path    => '/etc/php5/fpm/pool.d',
+      require => Package['PHP FPM and CGI']
   }
 
   if ($::osfamily == Debian) {
-    exec {"Fix FPM sources":
-      onlyif  => "test ! $(grep packages.dotdeb.org /etc/apt/sources.list.d/dotdeb.list)",
+    exec {'Fix FPM sources':
+      onlyif  => 'test ! $(grep packages.dotdeb.org /etc/apt/sources.list.d/dotdeb.list)',
       command => "echo \"deb http://packages.dotdeb.org ${::lsbdistcodename} all\" >> /etc/apt/sources.list.d/dotdeb.list; wget -O - www.dotdeb.org/dotdeb.gpg | sudo apt-key add -; apt-get update;",
     }
   } else {
-    exec {"Fix FPM sources":
-      onlyif  => "test ! true",
+    exec {'Fix FPM sources':
+      onlyif  => 'test ! true',
       command => 'test',
     }
   }
 
-  package {"PHP FPM and CGI":
-    require => [Package["PHP commons"], Exec["Fix FPM sources"]],
-    name    => ["php5-fpm", "php5-cgi"],
+  package {'PHP FPM and CGI':
     ensure  => installed,
+    name    => ['php5-fpm', 'php5-cgi'],
+    require => [Package['PHP commons'], Exec['Fix FPM sources']]
   }
 
-  service {"php5-fpm":
+  service {'php5-fpm':
     ensure      => running,
     hasstatus   => true,
     hasrestart  => true,
     enable      => true,
-    require     => Package["PHP FPM and CGI"],
+    require     => Package['PHP FPM and CGI']
   }
 
 }
